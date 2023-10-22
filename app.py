c3ea88a7e6db260d29081fd55de02046d7686f8f
Hiro <laciferin@gmail.com>
2023-10-22 08:24:43 +0530

    ui: ready
    Took 3 minutes

--- app.py
+++ app.py
@@ -23,35 +23,35 @@ if not app:
 
 qa_model = None
 
+st.title("Botech: Q/A assistant for slow learners")
 
 video = os.path.join("data", "Neurons and the brain.mp4")
+
 
 st.video(video, start_time=app.start_time)
 
 # Define a function to answer a question about a video
-def answer_question(question, video_url):
-    # Get the video snip of the explanation
-    video_snip =
-
-    # Answer the question using the QA model
-    answer = qa_model.answer(question, video_snip)
-
-    # Return the answer and the video snip
-    return answer, video_snip
+# def answer_question(question, video_url):
+#     # Get the video snip of the explanation
+#     video_snip =
+#
+#     # Answer the question using the QA model
+#     answer = qa_model.answer(question, video_snip)
+#
+#     # Return the answer and the video snip
+#     return answer, video_snip
 
 
 # Set the title of the app
-st.title("Learning Video with Question Answering")
 
 # Display the video
-video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 # Ask the user a question about the video
 question = st.text_input("Ask a question about the video:")
 
 # Answer the question and display the video snip
-if question:
-    answer, video_snip = answer_question(question, video_url)
-
-    st.write(answer)
-    st.image(video_snip)
+# if question:
+#     answer, video_snip = answer_question(question, video_url)
+#
+#     st.write(answer)
+#     st.image(video_snip)
