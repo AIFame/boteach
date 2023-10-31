ccf5c5be9f1bb15cb5249cb43e47ce340b6f1835
Hiro <laciferin@gmail.com>
2023-10-31 23:03:39 +0530

    fix: use video transcript instead of subtitles  (#8)
    * add: Neurons and the brain.txt
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes
    
    * utils: remove coed
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 13 seconds

--- utils/inputs/get_repo.py
+++ utils/inputs/get_repo.py
@@ -9,29 +9,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from config.constants import VIDEO_PATH, VIDEO_TRANSCRIPT_PATH
 
 
-def get_github_docs(repo_owner, repo_name):
-    with tempfile.TemporaryDirectory() as d:
-        subprocess.check_call(
-            f"git clone --depth 1 https://github.com/{repo_owner}/{repo_name}.git .",
-            cwd=d,
-            shell=True,
-        )
-        git_sha = (
-            subprocess.check_output("git rev-parse HEAD", shell=True, cwd=d)
-            .decode("utf-8")
-            .strip()
-        )
-        repo_path = pathlib.Path(d)
-        markdown_files = list(repo_path.glob("*/*.md")) + list(
-            repo_path.glob("*/*.mdx"),
-        )
-        for markdown_file in markdown_files:
-            with open(markdown_file) as f:
-                relative_path = markdown_file.relative_to(repo_path)
-                github_url = f"https://github.com/{repo_owner}/{repo_name}/blob/{git_sha}/{relative_path}"
-                yield Document(page_content=f.read(), metadata={"source": github_url})
-
-
 def get_text_chunk(text) -> [Document]:
     # use text_splitter to split it into documents list
     text_splitter = RecursiveCharacterTextSplitter(
